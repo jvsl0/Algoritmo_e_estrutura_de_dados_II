9323d9bb4a3865dd269e559a991f05b89f32663f
joaovictor404 <joaovictorlira05@gmail.com>
2025-10-22 10:10:24 -0300

    atividade 14

---
+++ atividades/atv_14.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+
+    char frase_inicio_luta[]={"Voce nunca saira daqui com vida!"};
+    char caixa_dialogo[100]={"(Silencio)"};
+    printf("Chefe antes da luta: %s\n", caixa_dialogo);
+
+    strcpy(caixa_dialogo, frase_inicio_luta);
+    printf("%s", frase_inicio_luta);
+
+}
